0a8164f26422c7120c7707b651ac917c32f93dd0
Uday Sen <vickysain817@gmail.com>
2025-12-23 14:28:42 +0530

    Update Header.jsx

--- frontened/src/components/Header/Header.jsx
+++ frontened/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='header-contents'>
-        <h2>Oreder your favorite food here</h2>
+        <h2>Order your favorite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is
             to satisfy your cravings and elevate your dining experience, one delicious meel at a time.
         </p>
